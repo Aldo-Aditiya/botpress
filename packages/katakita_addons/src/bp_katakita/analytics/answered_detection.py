5fcf6152e3d28cbfbefa7f6cd7d5355bb73d775f
Aldo-Aditiya <aldoan100@gmail.com>
2023-07-14 14:35:08 +0000

    Added preliminary code for topic and answered detection

---
+++ packages/katakita_addons/src/bp_katakita/analytics/answered_detection.py
@@ -0,0 +1,120 @@
+import os
+import json
+import re
+from typing import List, Optional
+from pprint import pprint
+
+from langchain.schema import OutputParserException
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+
+from chat_assistant.utils import load_azure_chat_openai
+from chat_assistant.callbacks import PromptCallbackHandler
+
+from bp_katakita.config import load_config
+from bp_katakita.chatgpt_nlu.model import NLUProcess, NLUDataSync
+
+# ----------------- #
+
+CONFIG = load_config()
+BOT_FILES_DIR = CONFIG["BOT_FILES_DIR"]
+
+prompt_callback_handler = PromptCallbackHandler()
+chat = load_azure_chat_openai(callback=prompt_callback_handler)
+
+# ----------------- #
+
+SYSTEM_PROMPT = """Assistant's task is to think step by step using the below CUSTOM_FORMAT delimited by triple backticks below:
+```
+thinking: Argue step by step based on the chat_history if the user question is answered. Emphasize newest messages.
+answered: <yes/no depending on previous step>
+```
+"""
+
+PRIMING_USER_MESSAGE_1 = """chat_history is delimited by triple backticks below.
+```
+User: Mau tanya tentang rover Mars
+Assistant: Apa yang ingin Anda ketahui tentang rover Mars? Saya dapat memberi tahu Anda tentang lokasinya atau statusnya.
+User: lokasinya
+Assistant: Rover Mars berada di Kater Jezero, dengan posisi spesifiknya dapat bervariasi.
+```
+
+answer in CUSTOM_FORMAT, delimited by triple backticks below:
+```
+thinking:
+"""
+
+PRIMING_ASSISTANT_MESSAGE_1 = """The user asked about the location of the Mars rover in Indonesian. The assistant responded that the Mars rover is in Jezero Crater, with the specific location being variable. Thus, the assistant provided the general area where the rover is located on Mars, which directly addresses the user's question.
+answered: yes
+```
+"""
+
+PRIMING_USER_MESSAGE_2 = """chat_history is delimited by triple backticks below.
+```
+User: Mau tanya tentang Nasa
+Assistant: Tentu! Apa yang ingin kamu tanyakan tentang NASA?
+User: Lokasinya
+Assistant: NASA (National Aeronautics and Space Administration) berlokasi di Amerika Serikat. 
+User: Kalau lokasi bank terdekat di mana?
+Assistant: Maaf, saya tidak memiliki akses langsung ke informasi terkini tentang lokasi bank terdekat.
+```
+
+answer in CUSTOM_FORMAT, delimited by triple backticks below:
+```
+thinking:
+"""
+
+PRIMING_ASSISTANT_MESSAGE_2 = """The user asked two questions about locations: one about NASA, and one about the nearest bank. The assistant answered the question about NASA correctly, stating it's located in the United States. However, when the user asked about the location of the nearest bank, the assistant indicated that it does not have direct access to real-time location information, thus not providing an actionable answer to the user's question.
+answered: no
+"""
+
+USER_MESSAGE_TEMPLATE = """chat_history is delimited by triple backticks below.
+```
+{chat_history}
+```
+
+answer in CUSTOM_FORMAT, delimited by triple backticks below:
+```
+thinking:
+"""
+
+# ----------------- #
+
+def parse_bool(text:str) -> bool:
+    if text == "yes":
+        return True
+    elif text == "no":
+        return False
+    else:
+        raise OutputParserException(f"Could not parse bool: {text}")
+
+def parse_output(text:str) -> dict:
+    pattern = r"answered: (.+)"
+    match = re.search(pattern, text, re.DOTALL)
+    if not match:
+        raise OutputParserException(f"Could not parse output: {text}")
+
+    reply_dict = {
+        "answered": parse_bool(match.group(1)),
+    }
+
+    return reply_dict
+
+def predict(chat_history:str):
+    # Construct Prompt
+    prompt = []
+    prompt.append(SystemMessage(content=SYSTEM_PROMPT))
+    prompt.append(HumanMessage(content=PRIMING_USER_MESSAGE_1))
+    prompt.append(AIMessage(content=PRIMING_ASSISTANT_MESSAGE_1))
+    prompt.append(HumanMessage(content=PRIMING_USER_MESSAGE_2))
+    prompt.append(AIMessage(content=PRIMING_ASSISTANT_MESSAGE_2))
+    prompt.append(HumanMessage(content=USER_MESSAGE_TEMPLATE.format(chat_history=chat_history)))
+
+    # Predict and Parse
+    result = chat(prompt)
+    result_dict = parse_output(result.content)
+
+    return result_dict
---
+++ packages/katakita_addons/src/bp_katakita/analytics/topic_detection.py
@@ -0,0 +1,106 @@
+import os
+import json
+import re
+from typing import List, Optional
+from pprint import pprint
+
+from langchain.schema import OutputParserException
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+
+from chat_assistant.utils import load_azure_chat_openai
+from chat_assistant.callbacks import PromptCallbackHandler
+
+from bp_katakita.config import load_config
+from bp_katakita.chatgpt_nlu.model import NLUProcess, NLUDataSync
+
+# ----------------- #
+
+CONFIG = load_config()
+BOT_FILES_DIR = CONFIG["BOT_FILES_DIR"]
+
+prompt_callback_handler = PromptCallbackHandler()
+chat = load_azure_chat_openai(callback=prompt_callback_handler)
+
+# ----------------- #
+
+SYSTEM_PROMPT = """Assistant's task is to think step by step using the below CUSTOM_FORMAT delimited by triple backticks below:
+```
+thinking: Argue step by step based on the chat_history what the topic is. Emphasize newest messages.
+topic: <one word determining the topic. Use Indonesian.>
+```
+"""
+
+PRIMING_USER_MESSAGE_1 = """chat_history is delimited by triple backticks below.
+```
+User: Mau tanya tentang rover Mars
+Assistant: Apa yang ingin Anda ketahui tentang rover Mars? Saya dapat memberi tahu Anda tentang lokasinya atau statusnya.
+```
+
+answer in CUSTOM_FORMAT, delimited by triple backticks below:
+```
+thinking:
+"""
+
+PRIMING_ASSISTANT_MESSAGE_1 = """The user is expressing interest in asking about the Mars rovers, as reflected in their question. I, as the assistant, offered to provide information regarding the Mars rovers' location or status. Hence, it's evident that the central subject of this conversation revolves around the Mars rovers.
+topic: Mars Rovers
+```
+"""
+
+PRIMING_USER_MESSAGE_2 = """chat_history is delimited by triple backticks below.
+```
+User: Lokasi Nasa di mana ya?
+Assistant: NASA (National Aeronautics and Space Administration) berlokasi di Amerika Serikat.
+```
+
+answer in CUSTOM_FORMAT, delimited by triple backticks below:
+```
+thinking:
+"""
+
+PRIMING_ASSISTANT_MESSAGE_2 = """Based on the chat history, the user is asking about the location of NASA. My response confirmed that NASA is located in the United States. The primary topic of the conversation can be inferred from this exchange.
+topic: NASA
+"""
+
+USER_MESSAGE_TEMPLATE = """chat_history is delimited by triple backticks below.
+```
+{chat_history}
+```
+
+answer in CUSTOM_FORMAT, delimited by triple backticks below:
+```
+thinking:
+"""
+
+# ----------------- #
+
+def parse_output(text:str) -> dict:
+    pattern = r"topic: (.+)"
+    match = re.search(pattern, text, re.DOTALL)
+    if not match:
+        raise OutputParserException(f"Could not parse output: {text}")
+
+    reply_dict = {
+        "topic": match.group(1),
+    }
+
+    return reply_dict
+
+def predict(chat_history:str):
+    # Construct Prompt
+    prompt = []
+    prompt.append(SystemMessage(content=SYSTEM_PROMPT))
+    prompt.append(HumanMessage(content=PRIMING_USER_MESSAGE_1))
+    prompt.append(AIMessage(content=PRIMING_ASSISTANT_MESSAGE_1))
+    prompt.append(HumanMessage(content=PRIMING_USER_MESSAGE_2))
+    prompt.append(AIMessage(content=PRIMING_ASSISTANT_MESSAGE_2))
+    prompt.append(HumanMessage(content=USER_MESSAGE_TEMPLATE.format(chat_history=chat_history)))
+
+    # Predict and Parse
+    result = chat(prompt)
+    result_dict = parse_output(result.content)
+
+    return result_dict
