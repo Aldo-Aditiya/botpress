8ad43d158e589b018ff2fc87285bdad3bde19e43
Aldo-Aditiya <aldoan100@gmail.com>
2023-07-19 15:38:52 +0000

    Initial Dashboard

---
+++ packages/katakita_addons/src/bp_katakita/dashboard/Dashboard.py
@@ -0,0 +1,137 @@
+from datetime import datetime, timedelta, timezone
+import pandas as pd
+
+import streamlit as st
+import plost
+from colorama import Fore, Back, Style
+
+from bp_katakita.utils.handler import chat_history as chat_history_handler
+from bp_katakita.config import load_config
+
+# ----------------- #
+
+# Assuming you have some sort of DataFrame
+data = {'column1': [1, 2, 3, 4, 5],
+        'column2': [10, 20, 30, 40, 50]}
+df = pd.DataFrame(data)
+
+# ----------------- #
+
+# Styling
+st.set_page_config(layout="wide")
+
+padding_top_html = """
+    <style>
+    .appview-container .main .block-container {
+            padding-top: 2rem;    
+            padding-bottom: 2rem; 
+        }
+    </style>
+"""
+st.markdown(padding_top_html, unsafe_allow_html=True)
+hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+card_style = """
+    <style>
+    div.css-12w0qpk {
+        background-color: #FFFFFF;
+        border: 1px solid #CCCCCC;
+        padding: 2% 2% 2% 2%;
+        border-radius: 5px;
+        
+        border-left: 0.5rem solid #9AD8E1 !important;
+        box-shadow: 0 0.15rem 1.75rem 0 rgba(58, 59, 69, 0.15) !important; 
+    }
+    label.css-mkogse.e16fv1kl2 {
+        color: #36b9cc !important;
+        font-weight: 700 !important;
+        text-transform: uppercase !important;
+    }
+    </style>
+"""
+st.markdown(card_style, unsafe_allow_html=True)
+
+chart_style = """
+<style>
+    div.chart-wrapper.fit-x.fit-y {
+        background-color: #FFFFFF;
+        border: 1px solid #CCCCCC;
+        padding: 5% 5% 0% 5%;
+        border-radius: 5px;
+        
+        border-left: 0.5rem solid #fa978c !important;
+        box-shadow: 0 0.15rem 1.75rem 0 rgba(58, 59, 69, 0.15) !important; 
+    }
+    canvas.marks {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: stretch;
+    }
+    </style>
+"""
+st.markdown(chart_style, unsafe_allow_html=True)
+
+# ----------------- #
+
+# Sidebar
+sidebar_bottom_html = f"""
+    <style>
+        [data-testid="stSidebarNav"] + div {{
+            position:relative;
+            background-position-x: center;
+            background-position-y: bottom;
+            height:50%;
+            background-size: 85% auto;
+            bottom:0;
+        }}
+    </style>
+"""
+st.sidebar.markdown(sidebar_bottom_html, unsafe_allow_html=True)
+
+# ----------------- #
+
+# Main Panel
+
+## Metrics
+df = chat_history_handler.read_as_df(query={"bot_id": "testing_18-07-23"})
+num_msg_exchanged = len(df)
+num_questions = len(df[df["author"] == "User"])
+num_conversations = len(df["session_id"].unique())
+num_unanswered_question = len(df[df["answered"] == "no"])
+percent_answered_question = 100 - (num_unanswered_question / num_questions) * 100
+
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Messages Exchanged", f"{num_msg_exchanged}")
+col2.metric("Questions", f"{num_questions}")
+col3.metric("Conversations", f"{num_conversations}")
+col4.metric("Answered Questions", f"{percent_answered_question:.2f}%")
+
+## Graphs
+df_convo_per_day = df.groupby(pd.Grouper(key='datetime', freq='D')).agg({'session_id': 'nunique'})
+df_messages_per_day = df.groupby(pd.Grouper(key='datetime', freq='D')).count()['message']
+topic_counts = df['topic'].value_counts().to_frame().reset_index().rename(columns={'index': 'topic'})
+topic_counts = topic_counts[topic_counts['topic'] != '']
+
+c1, c2 = st.columns((6,4), gap="medium")
+with c1:
+    st.markdown('#### Conversations')
+    st.line_chart(df_convo_per_day, use_container_width=True)
+with c2:
+    st.markdown('#### Topics')
+    plost.donut_chart(
+        data=topic_counts,
+        theta="count",
+        color="topic",
+        legend="bottom", 
+        use_container_width=True)
+st.markdown('#### Messages')
+st.line_chart(df_messages_per_day)
+
+    
---
+++ packages/katakita_addons/src/bp_katakita/dashboard/pages/1_Chat_History.py
@@ -0,0 +1,136 @@
+# Taken mostly from: https://blog.streamlit.io/auto-generate-a-dataframe-filtering-ui-in-streamlit-with-filter_dataframe/
+
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
+import pandas as pd
+import streamlit as st
+st.set_page_config(layout="wide", )
+
+from bp_katakita.utils.handler import chat_history as chat_history_handler
+
+# ----------------- #
+
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
+
+    Args:
+        df (pd.DataFrame): Original dataframe
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+
+    df = df.copy()
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in df.columns:
+        if is_object_dtype(df[col]):
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            # Treat columns with < 10 unique values as categorical
+            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
+                user_cat_input = right.multiselect(
+                    f"Values for {column}",
+                    df[column].unique(),
+                    default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Values for {column}",
+                    min_value=_min,
+                    max_value=_max,
+                    value=(_min, _max),
+                    step=step,
+                )
+                df = df[df[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Substring or regex in {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].astype(str).str.contains(user_text_input)]
+
+    return df
+
+# ----------------- #
+
+# Styling
+padding_top_html = """
+    <style>
+    .appview-container .main .block-container {
+            padding-top: 2rem;    
+            padding-bottom: 2rem; 
+        }
+    </style>
+"""
+st.markdown(padding_top_html, unsafe_allow_html=True)
+hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# ----------------- #
+
+# Sidebar
+sidebar_bottom_html = f"""
+    <style>
+        [data-testid="stSidebarNav"] + div {{
+            position:relative;
+            background-position-x: center;
+            background-position-y: bottom;
+            height:50%;
+            background-size: 85% auto;
+            bottom:0;
+        }}
+    </style>
+"""
+st.sidebar.markdown(sidebar_bottom_html, unsafe_allow_html=True)
+
+# ----------------- #
+
+# Main Panel
+
+st.markdown("### Chat History")   
+
+df = chat_history_handler.read_as_df(query={"bot_id": "testing_18-07-23"})
+df.drop(columns=['_id', 'message_id', 'bot_id'], inplace=True)
+df["datetime"] = pd.to_datetime(df["datetime"])
+df = df.rename(columns={'session_id': 'conversation_id'})
+
+st.dataframe(filter_dataframe(df))
